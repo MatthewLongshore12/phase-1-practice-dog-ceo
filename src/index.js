24e0cd65c156447fc6c6cda08c9a2d951c8866e5
Matthew Longshore <matthewlongshore12@gmail.com>
2023-02-06 08:40:25 -0500

    everything is done except challenge 4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+    
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+fetch(imgUrl) 
+.then(r => r.json())
+.then(data => data.message.forEach(dogImage => renderImage(dogImage)));
+
+fetch(breedUrl)
+.then(r => r.json())
+.then(data => {
+    let breeds = Object.keys(data.message)
+    renderBreeds(breeds)
+})
+
+
+function renderBreeds(breeds) {
+    const uList = document.getElementById('dog-breeds')
+    breeds.forEach(breed => {
+        const list = document.createElement('li')
+        list.innerText = breed
+        uList.append(list)
+        list.addEventListener('click', colorChange)
+    })
+}
+
+
+function colorChange(e) {
+    e.target.style.color = 'red'
+}
+
+
+
+
+
+function renderImage(dogImage) {
+    const container = document.getElementById('dog-image-container')
+    const img = document.createElement('img')
+    img.src = dogImage
+    container.append(img)
+}
+
+})
